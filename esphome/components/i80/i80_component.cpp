a60551a28f2a43211ecf5a8afc10b660e997f17d
Kilrah <kilrah@kilrah.xyz>
2026-02-20 11:28:12 +0100

    Add and fix i80/io_bus from embedme

---
+++ esphome/components/i80/i80_component.cpp
@@ -0,0 +1,19 @@
+#include "i80_component.h"
+#ifdef USE_I80
+
+namespace esphome {
+namespace i80 {
+void I80Component::dump_config() {
+  ESP_LOGCONFIG(TAG, "I80 bus:");
+  LOG_PIN("  WR Pin: ", this->wr_pin_);
+  LOG_PIN("  DC Pin: ", this->dc_pin_);
+  unsigned i;
+  for (i = 0; i != this->data_pins_.size(); i++) {
+    ESP_LOGCONFIG(TAG, "  Data pin %u: GPIO%d", i, this->data_pins_[i]);
+  }
+}
+
+}  // namespace i80
+}  // namespace esphome
+
+#endif  // USE_I80
---
+++ esphome/components/io_bus/__init__.py
@@ -0,0 +1,4 @@
+import esphome.codegen as cg
+
+io_bus_ns = cg.esphome_ns.namespace("io_bus")
+IOBus = io_bus_ns.class_("IOBus")
